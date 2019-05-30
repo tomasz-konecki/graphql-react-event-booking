fb4b6eec00aefbd1d8baf8fe5c45e1a4acec47b1
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2019-05-30 10:37:26 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const events = [];
+
+app.use(bodyParser.json());
+
+app.use(
+	"/graphql",
+	graphqlHttp({
+		schema: buildSchema(`
+      type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
+
+      input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
+
+      type RootQuery {
+        events: [Event!]!
+      }
+
+      type RootMutation {
+        createEvent(eventInput: EventInput): Event
+      }
+
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
+    `),
+		rootValue: {
+			events: () => {
+				return events;
+			},
+			createEvent: args => {
+				const event = {
+					_id: Math.random().toString(),
+					title: args.eventInput.title,
+					description: args.eventInput.description,
+					price: +args.eventInput.price,
+					date: args.eventInput.date
+				};
+				events.push(event);
+				return event;
+			}
+		},
+		graphiql: true
+	})
+);
+
+mongoose
+	.connect(
+		`mongodb+srv://${process.env.MONGO_USER}:${
+			process.env.MONGO_PASSWORD
+		}@cluster0-pzzsp.gcp.mongodb.net/test?retryWrites=true`
+	)
+	.then(() => {
+		app.listen(3000);
+	})
+	.catch(err => {
+		console.log(err);
+	});
