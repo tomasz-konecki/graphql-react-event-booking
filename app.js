53d97066625e647dfd40f71ce03e44781bf5fc99
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2019-05-30 11:14:09 +0200

    Update repo

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const graphqlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
 
-const app = express();
+const Event = require("./models/event");
 
-const events = [];
+const app = express();
 
 app.use(bodyParser.json());
 
@@ -44,18 +44,34 @@ app.use(
     `),
 		rootValue: {
 			events: () => {
-				return events;
+				return Event.find()
+					.then(events => {
+						return events.map(event => {
+							return { ...event._doc };
+						});
+					})
+					.catch(err => {
+						throw err;
+					});
 			},
 			createEvent: args => {
-				const event = {
-					_id: Math.random().toString(),
+				const event = new Event({
 					title: args.eventInput.title,
 					description: args.eventInput.description,
 					price: +args.eventInput.price,
-					date: args.eventInput.date
-				};
-				events.push(event);
-				return event;
+					date: new Date(args.eventInput.date)
+				});
+
+				return event
+					.save()
+					.then(result => {
+						console.log(result);
+						return { ...result._doc };
+					})
+					.catch(err => {
+						console.log(err);
+						throw err;
+					});
 			}
 		},
 		graphiql: true
@@ -66,7 +82,7 @@ mongoose
 	.connect(
 		`mongodb+srv://${process.env.MONGO_USER}:${
 			process.env.MONGO_PASSWORD
-		}@cluster0-pzzsp.gcp.mongodb.net/test?retryWrites=true`
+		}@cluster0-pzzsp.gcp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`
 	)
 	.then(() => {
 		app.listen(3000);
